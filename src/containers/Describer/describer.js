f050ba1c16ed8f9fe760616d131bc48b237ec83d
LetMeCode12 <norbertskulski@wp.pl>
2021-01-17 15:42:24 +0100

    add

--- src/containers/Describer/describer.js
+++ src/containers/Describer/describer.js
@@ -5,24 +5,22 @@ import Aos from "aos";
 
 class Describer extends Component {
 
-    componentDidMount(){
-        Aos.init({
-            duration:2000
-        })
-    }
+  componentDidMount() {
+    Aos.init({
+      duration: 2000
+    })
+  }
 
-    render() {
-        return (
-            <div className="Describer">
-                <div className="Content">
-                   <p>Witam na mojej stronie. Nazywam sie Norbert, ukończyłem pierwszy stopień na Uniwersytecie Technologiczno-Przyrodniczy im. Jana i Jędrzeja Śniadeckich w Bydgoszczy.<img className="UtpLogo" data-aos="fade-left" src={utp} /></p>
-                   <p>a</p>   
-                   <p>asdasd</p>    
-                </div>
-            </div>
+  render() {
+    return (
+      <div className="Describer">
+        <div className="Content">
+          <p>Witam na mojej stronie. Nazywam sie Norbert, ukończyłem pierwszy stopień na Uniwersytecie Technologiczno-Przyrodniczy im. Jana i Jędrzeja Śniadeckich w Bydgoszczy.</p><img className="UtpLogo" data-aos="fade-left" src={utp} />
+        </div>
+      </div>
 
-        )
-    }
+    )
+  }
 }
 
 export default Describer
