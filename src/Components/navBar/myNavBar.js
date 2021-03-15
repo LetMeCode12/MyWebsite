669ec1b0a8d1bb49531f5d7e3ed6b8c9250eef55
LetMeCode12 <you@example.com>
2021-03-15 15:58:49 +0100

    add

--- src/Components/navBar/myNavBar.js
+++ src/Components/navBar/myNavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect ,useRef} from 'react'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import {withRouter} from "react-router-dom";
 import "./myNavBar.scss"
@@ -6,8 +6,35 @@ import Underline from '../underline/underline';
 import Logo from "../../Graphics/utp.png"
 
 function MyNavBar(props) {
+
+    let  myRef = useRef();
+
+    const setSticky=()=>{
+        myRef.current.style.position="sticky";
+        document.documentElement.style.setProperty('--bgColor',"rgba(0, 0, 0, 0.5)");
+        document.documentElement.style.setProperty('--hbgColor',"rgba(0, 0, 0, 0.7)");     
+    }
+
+    const setRelative=()=>{
+        myRef.current.style.position="relative";      
+        document.documentElement.style.setProperty('--bgColor',"linear-gradient(to left,rgba(99, 209, 252, 0.521),rgba(0, 174, 255, 1))");  
+        document.documentElement.style.setProperty('--hbgColor',"rgb(0, 150, 219)");     
+    }
+
+    useEffect(()=>{
+        setRelative();
+        window.onscroll=()=>{
+            if(window.scrollY>300){
+                  setSticky(); 
+            }else{
+                  setRelative();
+            }
+        }
+    })
+
+
     return (
-        <Navbar className="myNavBar" bg="dark" expand="lg" variant="dark">
+        <Navbar className="myNavBar" bg="none" expand="lg" variant="dark" ref={myRef}>
             <Navbar.Brand ><div className="Title"><img className="Logo" src={Logo}/>Witaj</div></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
